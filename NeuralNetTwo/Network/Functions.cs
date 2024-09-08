6e164ba7e29b7fd4687535af3e3d6aa3b59ed036
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-08 14:02:03 -0400

    Mean squared error loss function

--- NeuralNetTwo/Network/Functions.cs
+++ NeuralNetTwo/Network/Functions.cs
@@ -11,5 +11,16 @@ public static class funcs {
         return 1/(1 + Math.Pow(constants.e, -1 * x));
     }
 
+    public static double meanSquaredError(double[] trainingDataInputs, double[] trainingDataAnswers, network n) {
+        double mse = 0;
+
+        double[] networkOutput = n.calculateOutput(trainingDataInputs);
+
+        for(int i = 0; i < trainingDataInputs.Length; i++) {
+            mse += Math.Pow(trainingDataAnswers[i] - networkOutput[i], 2);
+        }
+
+        return mse;
+    }
 
 }
