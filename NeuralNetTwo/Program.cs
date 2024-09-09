d78786ba1762d90d43c181d432e847fff4d4f67c
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-09 14:46:20 -0400

    More data

--- NeuralNetTwo/Program.cs
+++ NeuralNetTwo/Program.cs
@@ -9,8 +9,13 @@
             int[] layersizes = {3, 4, 6};
             network n = new network(layersizes);
 
-            funcs.train(n, trainingIns, trainingOuts, 0.1, 6);
+            funcs.train(n, trainingIns, trainingOuts, 0.3, 30);
             Console.WriteLine(funcs.classify(n.calculateOutput([255, 0, 0])));
+            Console.WriteLine(funcs.classify(n.calculateOutput([0, 255, 0])));
+
+            for(int i = 0; i < n.errorHistory.Count; i++) {
+                Console.Write(Math.Round(n.errorHistory[i], 2));
+            }
         }
     }
 }
